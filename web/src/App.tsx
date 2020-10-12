5ca4f0ba08f004a5102a8f18529982bd0ed4fe50
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-12 08:37:23 -0300

    :lipstick: chore: simple styles from landing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div>Hello World</div>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Tubarão</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <a href="" className="enter-app">
+          >
+        </a>
+      </div>
+    </div>
   );
 }
 
