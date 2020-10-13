b70896c6830c6d1f86ce7ee1f28228053f5c0d81
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-12 22:01:05 -0300

    :fire: chore: adding leaflet map and tilelayer whith mapbox

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {FiPlus} from 'react-icons/fi';
 
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/Orphanages-map.css';
@@ -23,7 +27,16 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div></div>
+      <Map 
+        center={[-28.4706384, -49.0312173]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url={`
+          https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}
+        `} />
+      </Map>
 
       <Link to="/" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
