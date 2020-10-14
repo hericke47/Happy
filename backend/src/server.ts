84d9a69c7f57f89a3b267e2f41e74a833160fa91
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-13 23:45:40 -0300

    ✨ chore: add validantion of data

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
 app.listen(3333);
