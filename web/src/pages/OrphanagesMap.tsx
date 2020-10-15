ca6c9bc2ba30394f3be0389b872a0a738b3e78d0
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-15 09:14:55 -0300

    💄 style: add some pages of create an orphanage

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -51,14 +51,14 @@ function OrphanagesMap() {
         >
           <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
             Lar das meninas
-            <Link to="">
+            <Link to="/orphanages/1">
               <FiArrowRight size={20} color='#fff'/>
             </Link>
           </Popup>
         </Marker>
       </Map>
 
-      <Link to="/" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
