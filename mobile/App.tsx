274ccacb084d4510015459b2843270f23b047e1c
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-16 11:29:05 -0300

    :fire: chore: add map and iamges from marker on mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE} 
+        style={styles.map} 
+        initialRegion={{
+          latitude: -28.4706384,
+          longitude: -49.0312173,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }} 
+      >
+        <Marker 
+          icon={mapMarker}
+          coordinate={{
+            latitude: -28.4706384,
+            longitude: -49.0312173,
+          }}
+        />
+      </MapView>
     </View>
   );
 }
@@ -14,8 +33,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
