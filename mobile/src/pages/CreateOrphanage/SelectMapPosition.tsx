6b48e4a2ac2b29b8a91dad1d950b8dc93aacde71
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-18 13:26:16 -0300

    :bento: chore: select map position on mobile

--- mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -3,15 +3,21 @@ import { View, StyleSheet, Dimensions, Text } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { MapEvent, Marker } from 'react-native-maps';
 
 import mapMarkerImg from '../../images/map-marker.png';
+import { useState } from 'react';
 
 export default function SelectMapPosition() {
   const navigation = useNavigation();
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
 
   function handleNextStep() {
     navigation.navigate('OrphanageData');
+  }
+
+  function handleSelectMapPosition(event: MapEvent) {
+    setPosition(event.nativeEvent.coordinate);
   }
 
   return (
@@ -24,11 +30,14 @@ export default function SelectMapPosition() {
           longitudeDelta: 0.008,
         }}
         style={styles.mapStyle}
+        onPress={handleSelectMapPosition}
       >
-        <Marker 
-          icon={mapMarkerImg}
-          coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
-        />
+        { position.latitude !== 0 && (
+          <Marker 
+            icon={mapMarkerImg}
+            coordinate={{ latitude: position.latitude, longitude: position.longitude }}
+          />
+        )}
       </MapView>
 
       <RectButton style={styles.nextButton} onPress={handleNextStep}>
