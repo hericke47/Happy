09778a8e72e3e5de5c228f39d713f5f9d9964aff
Herick Exterkoetter <herickherick47@gmail.com>
2020-10-27 20:38:42 -0300

    :pushpin: chore: finished authentication users

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,12 +3,17 @@ import multer from 'multer';
 
 import uploadConfig from './config/upload';
 import OrphanagesController from './controllers/OrphanagesController';
+import UsersController from './controllers/UsersController';
+
+import ensureAuthenticated from "./middlewares/ensureAuthenticated";
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
+routes.post('/orphanages', ensureAuthenticated, upload.array('images') , OrphanagesController.create);
+routes.post('/users', UsersController.create);
+routes.post('/sessions', UsersController.login);
 
 export default routes;
